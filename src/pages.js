a09d36b856893f07d6db796f958d1b07eadd30b7
Skye Lucking <lucking.skye@gmail.com>
2021-04-06 20:45:19 -0700

    added the router and history

---
+++ src/pages.js
@@ -0,0 +1,39 @@
+import React from "react"
+
+export function Home(){
+    return(
+        <div>
+            <h1>Skye Lucking Bootcamp Portfolio</h1>
+        </div>
+    )
+}
+
+export function About(){
+    return(
+        <div>
+            <h1>Skye Lucking ~ About</h1>
+        </div>
+    )
+}
+
+export function Resume(){
+    return(
+        <div>
+            <h1>Resume</h1>
+        </div>
+    )
+}
+export function Contact(){
+    return(
+        <div>
+            <h1>Contact</h1>
+        </div>
+    )
+}
+export function Portfolio(){
+    return(
+        <div>
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
