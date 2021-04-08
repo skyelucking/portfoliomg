722f3c5826a6bddf21b545639e136a2ee450731e
Skye Lucking <lucking.skye@gmail.com>
2021-04-07 22:10:15 -0700

    created a responsive to navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { Home, About, Resume, Contact, Portfolio, Whoops404, CodingBootcamp,  UnityDev, InstructionalDesign } from "./pages";
+import Navbar from "./Components/Navbar"
 
 function App() {
   return (
+    
+      
+  
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ src/Components/Navbar.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import "../App.css";
+import ReorderIcon from "@material-ui/icons/Reorder";
+
+
+function Navbar() {
+  const [showLinks, setShowLinks] = useState(false);
+
+  return (
+    <div className="Navbar">
+      <div className="leftside">
+        <div className="links" id={showLinks ? "hidden" : ""}>
+          <a href="/index.html">Home</a>
+          <a href="/about">About</a>
+          <a href="/contact">Contact</a>
+          <a href="resume">Resume</a>
+          <a href="portfolio">Portfolio</a>
+        </div>
+        <button onClick={() => setShowLinks(!showLinks)}>
+          {" "}
+          <ReorderIcon />
+        </button>
+      </div>
+      <div className="rightSide"></div>
+    </div>
+  );
+}
+
+export default Navbar;
